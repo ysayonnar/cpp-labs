08cd4b815dcd81433fdd4d04ac84e807368fabb3
ysayonnar <ysayonnar@gmail.com>
2025-10-22 20:30:23 +0300

    upd: finished lab3 for kova

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -14,12 +14,13 @@ int main() {
     int main_choice;
     int number_device;
     do {
+        std::cout << std::endl;
         smartphone_obj[0].print_header();
         for (int i = 0; i < amount; i++) {
             std::cout << smartphone_obj[i] << std::endl;
         }
 
-        std::cout << "What device you want to change:\t";
+        std::cout << std::endl << "What device you want to change:\t";
         std::cin >> number_device;
         if (number_device < 0 || number_device >= amount) {
             std::cout << "max value is " << amount << " and min value is 0" << std::endl;
@@ -29,28 +30,4 @@ int main() {
         }
 
     } while (main_choice != -1);
-
-    // PortableMachine portable_obj;
-    // PersonalComputer pc_obj;
-    // SmartPhone smartphone_obj;
-    // Laptop laptop_obj;
-    // Monoblock monoblock_obj;
-
-    // int main_choice;
-    // do {
-    //     std::cout << "\nChoose type of device:\n1. PortableMachine\n2. PersonalComputer\n3. SmartPhone\n4. Laptop\n5. Monoblock\n0. Exit\n> ";
-    //     std::cin >> main_choice;
-    //     if (main_choice == 1) {
-    //         edit_portable(portable_obj);
-    //     } else if (main_choice == 2) {
-    //         edit_pc(pc_obj);
-    //     } else if (main_choice == 3) {
-    //         edit_smartphone(smartphone_obj);
-    //     } else if (main_choice == 4) {
-    //         edit_laptop(laptop_obj);
-    //     } else if (main_choice == 5) {
-    //         edit_monoblock(monoblock_obj);
-    //     }
-    // } while (main_choice != 0);
-    // return 0;
 }
